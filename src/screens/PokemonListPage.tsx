1cf3fdec9e200682d8b30686e55953bc66f19085
Eric Navar <ericn2413@gmail.com>
2025-10-30 08:35:59 -0400

    ednavar: filter pokemon based in user search

--- src/screens/PokemonListPage.tsx
+++ src/screens/PokemonListPage.tsx
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { tss } from '../tss';
-import { useGetPokemons } from 'src/hooks/useGetPokemons';
+import { Pokemon, useGetPokemons } from 'src/hooks/useGetPokemons';
 import { PokemonListItem } from 'src/components/PokemonListItem';
 import { IndeterminateProgressIndicator } from 'src/components/IndeterminateProgressIndicator';
 
+// filters based on name
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+const filterPokemonData = (data: Pokemon[], searchString: string) => {
+  // TODO: simplify. maybe just use simple indexOf
+  const regex = new RegExp(searchString.trim(), 'i');
+  return data.filter((pokemon) => regex.test(pokemon.name));
+};
+
 export const PokemonListPage = () => {
   const { classes } = useStyles();
+  const [searchString, setSearchString] = useState<string>('');
   const { data, loading, error } = useGetPokemons();
 
-  if (loading) {
+  const filteredData = useMemo(() => filterPokemonData(data, searchString), [data, searchString]);
+
+  const onChangeSearchString = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchString(e.currentTarget.value);
+  };
+
+  const generateSearchResults = () => {
+    if (loading) {
+      return <IndeterminateProgressIndicator />;
+    }
+    if (error) {
+      return <p>There was an error.</p>;
+    }
+    if (!filteredData?.length) {
+      return <p>There is no data.</p>;
+    }
     return (
-      <div className={classes.root}>
-        <IndeterminateProgressIndicator />
-      </div>
-    );
-  }
-  if (error) {
-    return <div className={classes.root}>There was an error.</div>;
-  }
-  if (!data?.length) {
-    return <div className={classes.root}>There is no data.</div>;
-  }
-  return (
-    <div className={classes.root}>
-      <ul>
-        {data.map((d) => (
+      <li>
+        {filteredData.map((d) => (
           <PokemonListItem key={d.id} data={d} />
         ))}
-      </ul>
+      </li>
+    );
+  };
+
+  return (
+    <div className={classes.root}>
+      <input type="text" value={searchString} onChange={onChangeSearchString} />
+      <ul>{generateSearchResults()}</ul>
     </div>
   );
 };
